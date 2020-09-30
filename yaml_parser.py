fcb4250e0018eeac5fc6000bc7033f72f696ca64
Rafal <hajtpl@gmail.com>
2020-09-30 11:25:28 +0200

    Added jsonschema validation of yaml config file

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,10 +1,62 @@
 import yaml
-from jsonschema import validate
+import json
+from jsonschema import validate, ValidationError
 import logging
 
 class YamlParser:
 
     def __init__(self, path):
+
+        self.content = None
+        self.slack_webhook_url = ""
+        self.logfile_path = ""
+        self.database_path = ""
+        self.filters = {}
+        self.schema = {
+            "type": "object",
+            "required": ["slack", "database", "logfile", "filters"],
+            "properties": {
+                "slack": {
+                    "type": "object",
+                    "required": ["webhook_url"],
+                    "properties": {
+                        "webhook_url": {
+                            "type": "string"
+                        } 
+                    }
+                },
+                "database": {
+                    "type": "object",
+                    "required": ["path"],
+                    "properties": {
+                        "path": {
+                            "type": "string"
+                        } 
+                    }
+                },
+                "logfile": {
+                    "type": "object",
+                    "required": ["path"],
+                    "properties": {
+                        "path": {
+                            "type": "string"
+                        } 
+                    }
+                },
+                "filters": {
+                    "type": "object",
+                    "required": ["olx"],
+                    "properties": {
+                        "olx": {
+                            "type": "array",
+                            "items": {
+                                "type": "string"
+                            }
+                        } 
+                    }
+                }
+            }
+        }
 
         try:
             with open(path, 'r') as file:
@@ -13,8 +65,13 @@ class YamlParser:
                 except yaml.YAMLError as err:
                     logging.error(err)
                 else:
-                    self.content = self._validate_is_dict(content)
-                    self._parse_content()
+                    try:
+                        validate(content, self.schema)
+                    except ValidationError as err:
+                        logging.error(err)
+                    else:
+                        self.content = content
+                        self._parse_content()
         except FileNotFoundError:
             logging.error(f"No such file or directory: '{path}'")
         except IsADirectoryError:
@@ -26,146 +83,20 @@ class YamlParser:
 
 
     def __repr__(self):
-        return str(self.content)
+        return json.dumps(self.content, sort_keys=False)
 
 
-    def _validate_is_dict(self, target):
-        if type(target) is type(dict()):
-            return target
-        else:
-            logging.error("Wrong format of config data in config file.")
-            raise TypeError(f"'{type(target).__name__}' object, should be 'dict'")
-
-
-    def _validate_is_string(self, target):
-        if type(target) is type(str()):
-            return target
-        else:
-            logging.error("Wrong format of config data in config file.")
-            raise TypeError(f"'{type(target).__name__}' object, should be 'str'")
-
-
-    def _validate_is_int(self, target):
-        if type(target) is type(int()):
-            return target
-        else:
-            logging.error("Wrong format of config data in config file.")
-            raise TypeError(f"'{type(target).__name__}' object, should be 'int'")
-
-
-    def _validate_is_list(self, target):
-        if type(target) is type(list()):
-            return target
-        else:
-            logging.error("Wrong format of config data in config file.")
-            raise TypeError(f"'{type(target).__name__}' object, should be 'list'")
-
-
-    def _get_slack_config(self):
-        key = 'slack'
-        slack = self.content.get(key)
-        self._validate_is_dict(slack)
-        return slack 
-
-    
-    def _get_slack_webhook_url(self):
-        key = 'webhook_url'
-        slack = self._get_slack_config()
-        webhook_url = slack.get(key)
-        self._validate_is_string(webhook_url)
-        return webhook_url  
-
-
-    def _get_logfile_config(self):
-        key = 'logfile'
-        logfile = self.content.get(key)
-        self._validate_is_dict(logfile)
-        return logfile    
-        
-
-    def _get_logfile_path(self):
-        key = 'path'
-        logfile = self._get_logfile_config()
-        path = logfile.get(key)
-        self._validate_is_string(path)
-        return path
-
-
-    def _get_database_config(self):
-        key = 'database'
-        database = self.content.get(key)
-        self._validate_is_dict(database)
-        return database    
-        
-
-    def _get_database_path(self):
-        key = 'path'
-        database = self._get_database_config()
-        path = database.get(key)
-        self._validate_is_string(path)
-        return path
-                
-
-    def _get_facebook_config(self):
-        key = 'facebook'
-        facebook = self.content.get(key)    
-        self._validate_is_dict(facebook)
-        return facebook 
-
-
-    def _get_facebook_email(self):
-        key = 'email'
-        facebook = self._get_facebook_config()
-        email = facebook.get(key)
-        self._validate_is_string(email)
-        return email
-
-
-    def _get_facebook_password(self):
-        key = 'password'
-        facebook = self._get_facebook_config()
-        password = facebook.get(key)
-        self._validate_is_string(password)
-        return password
-
-
-    def _get_facebook_friend_id(self):
-        key = 'friend_id'
-        facebook = self._get_facebook_config()
-        friend_id = facebook.get(key)
-        self._validate_is_int(friend_id)
-        return friend_id
-
-
-    def _get_filters_data(self):
-        key = 'filters'
-        filters = self.content.get(key)    
-        self._validate_is_dict(filters)
-        return filters 
-
-
-    def _get_olx_filters(self):
-        key = 'olx'
-        filters = self._get_filters_data()
-        olx = filters.get(key)
-        self._validate_is_list(olx)
-        return olx
+    def _get_nested_value(self, *keys):
+        """ Function which extract nested value from dict structure. """
+        value = self.content
+        for key in keys:
+            value = value.get(key)
+        return value
 
 
     def _parse_content(self):
-        self.slack = self._get_slack_config()
-        self.slack_webhook_url = self._get_slack_webhook_url()
-        self.logfile = self._get_logfile_config()
-        self.logfile_path = self._get_logfile_path()
-        self.database = self._get_database_config()
-        self.database_path = self._get_database_path()
-        self.facebook = self._get_facebook_config()
-        self.facebook_email = self._get_facebook_email()
-        self.facebook_password = self._get_facebook_password()
-        self.facebook_friend_id = self._get_facebook_friend_id()
-        self.filters = self._get_filters_data()
-        self.olx_filters = self._get_olx_filters()
-
-
-if __name__ == "__main__":
-    yaml_file = YamlParser("config.yaml")
+        """ Function which gets and setups needed config data from file content. """
+        self.slack_webhook_url = self._get_nested_value('slack', 'webhook_url')
+        self.logfile_path = self._get_nested_value('logfile', 'path')
+        self.database_path = self._get_nested_value('database', 'path')
+        self.filters = self.content.get('filters')
